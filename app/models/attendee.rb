11ba7a558f68eddd963f1cd3694ae660c44f3a69
OtiAnn <anniebh30@gmail.com>
2014-08-20 01:20:25 +0800

    Modified association

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,6 @@
 class Attendee < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+  has_many :field_values
+  has_many :fields, through: :form
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,3 +1,4 @@
 class Field < ActiveRecord::Base
   belongs_to :event
+  has_many :field_values
 end
